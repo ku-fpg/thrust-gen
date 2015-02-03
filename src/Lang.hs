1d4137d9d01f8fe2a9b5158e3e2275dc3e5a236e
superfunc <superfunc@protonmail.ch>
2015-02-02 19:36:31 -0600

    fixing mike mistakes

---
+++ src/Lang.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE GADTs, KindSignatures, StandaloneDeriving, DeriveFunctor #-}
+
+module Lang where
+
+import Control.Monad.State
+import Control.Monad.Free
+import Types
+import Data.List
+
+new :: Body Int
+new = do p <- get
+         put (p+1)
+         return p
+
+int :: ElemType
+int = CInt
+
+proc :: ElemType -> String -> [(ElemType, String)] -> Body () -> Prog ()
+proc t name args body = liftF $ Proc t name args body ()
+
+vector :: Int -> [(Int, Int)] -> Body HVector
+vector sz elems = do p <- new
+                     let vector = Vec p sz elems
+                     liftF $ Decl (vector) vector
+
+transform :: Name -> HVector -> Body ()
+transform fun col = liftF $ Trans fun col ()
+
+
+run :: Prog () -> IO()
+run p = do res <- runLibs p
+           putStrLn $ concatMap (++"\n") $ nub res
+           runInterp p
+
+           where 
+                getLibs :: Stmt a -> IO [String]
+                getLibs (Free (Decl v next)) = liftM2 (++) (return ["#include thrust/host_vector.h"]) (getLibs next)
+                getLibs (Free (Trans fun v next)) = liftM2 (++)  (return ["#include thrust/transform.h"]) (getLibs next)
+                getLibs (Pure _) = return []
+                
+                runLibs :: Prog () -> IO [String]
+                runLibs (Free (Proc t name args body next)) = liftM2 (++) (getLibs (evalStateT body 0)) (runLibs next)
+                runLibs (Pure _) = return []
+                
+                interp :: Stmt a -> IO()
+                interp (Free a@(Decl v next)) = putStrLn (show a) >> interp next
+                interp (Free t@(Trans fun v next)) = putStrLn (show t) >> interp next
+                interp (Pure _)    = putStr ""
+                
+                runInterp :: Prog () -> IO ()
+                runInterp (Free p@(Proc t name args body next)) = putStrLn (show p) >> interp (evalStateT body 0) >> runInterp next 
+                runInterp (Pure _) = putStrLn "}"
---
+++ src/Types.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE GADTs, KindSignatures, StandaloneDeriving, DeriveFunctor #-}
+
+module Types where
+
+
+import Control.Monad.State
+import Control.Monad.Free
+
+data HVector = Vec Int Int [(Int, Int)]
+
+data ElemType where
+    CInt :: ElemType
+
+data Statement next = Decl HVector next | Trans String HVector next
+    deriving (Functor)
+
+data Fun next = Proc ElemType Name [(ElemType, Name)] (Body next) next
+    deriving (Functor)
+
+instance Show (Statement next) where
+    show (Decl (Vec ident sz elems) next) = "\tthrust::host_vector<type>v" ++ (show ident) ++ ";"
+    show (Trans fun (Vec ident sz elems) next) = "\tthrust::transform(v" ++ show ident ++ ".begin(), v" ++ show ident ++ ".end(), " ++ fun ++ ");"
+
+instance Show ElemType where
+    show (CInt) = "int"    
+
+instance Show (Fun next) where
+    show (Proc t name args body next) = show t ++ " " ++ name ++
+                                        "(" ++ (foldr (\(t, name) acc -> 
+                                                        (show t ++ " " ++ name) ++ 
+                                                        case acc of
+                                                           ")" -> acc
+                                                           _ -> ", " ++ acc)
+                                                       ")" args) 
+                                               ++ "{"
+
+type Name = String
+
+type Stmt = Free Statement
+
+type Body = StateT Int Stmt
+
+type Prog = Free Fun
+
