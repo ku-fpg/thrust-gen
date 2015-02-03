dfac9a5f574df106ac1fc2f50be38880b638a9ef
superfunc <superfunc@protonmail.ch>
2015-02-02 19:44:24 -0600

    refactor some functions

--- src/Lang.hs
+++ src/Lang.hs
@@ -26,27 +26,25 @@ vector sz elems = do p <- new
 transform :: Name -> HVector -> Body ()
 transform fun col = liftF $ Trans fun col ()
 
+getLibs :: Stmt a -> IO [String]
+getLibs (Free (Decl v next)) = liftM2 (++) (return ["#include thrust/host_vector.h"]) (getLibs next)
+getLibs (Free (Trans fun v next)) = liftM2 (++)  (return ["#include thrust/transform.h"]) (getLibs next)
+getLibs (Pure _) = return []
+
+runLibs :: Prog () -> IO [String]
+runLibs (Free (Proc t name args body next)) = liftM2 (++) (getLibs (evalStateT body 0)) (runLibs next)
+runLibs (Pure _) = return []
+
+interp :: Stmt a -> IO()
+interp (Free a@(Decl v next)) = putStrLn (show a) >> interp next
+interp (Free t@(Trans fun v next)) = putStrLn (show t) >> interp next
+interp (Pure _)    = putStr ""
+
+runInterp :: Prog () -> IO ()
+runInterp (Free p@(Proc t name args body next)) = putStrLn (show p) >> interp (evalStateT body 0) >> runInterp next 
+runInterp (Pure _) = putStrLn "}"
 
 run :: Prog () -> IO()
 run p = do res <- runLibs p
            putStrLn $ concatMap (++"\n") $ nub res
            runInterp p
-
-           where 
-                getLibs :: Stmt a -> IO [String]
-                getLibs (Free (Decl v next)) = liftM2 (++) (return ["#include thrust/host_vector.h"]) (getLibs next)
-                getLibs (Free (Trans fun v next)) = liftM2 (++)  (return ["#include thrust/transform.h"]) (getLibs next)
-                getLibs (Pure _) = return []
-                
-                runLibs :: Prog () -> IO [String]
-                runLibs (Free (Proc t name args body next)) = liftM2 (++) (getLibs (evalStateT body 0)) (runLibs next)
-                runLibs (Pure _) = return []
-                
-                interp :: Stmt a -> IO()
-                interp (Free a@(Decl v next)) = putStrLn (show a) >> interp next
-                interp (Free t@(Trans fun v next)) = putStrLn (show t) >> interp next
-                interp (Pure _)    = putStr ""
-                
-                runInterp :: Prog () -> IO ()
-                runInterp (Free p@(Proc t name args body next)) = putStrLn (show p) >> interp (evalStateT body 0) >> runInterp next 
-                runInterp (Pure _) = putStrLn "}"
