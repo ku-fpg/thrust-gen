71fd2aa0fbcc8dd54acfb009c847ef48b6980b06
superfunc <superfunc@protonmail.ch>
2015-02-04 17:08:49 -0600

    cleanup

--- src/Types.hs
+++ src/Types.hs
@@ -18,21 +18,34 @@ data Fun next = Proc ElemType Name [(ElemType, Name)] (Body next) next
     deriving (Functor)
 
 instance Show (Statement next) where
-    show (Decl (Vec ident sz elems) next) = "\tthrust::host_vector<type>v" ++ (show ident) ++ ";"
-    show (Trans fun (Vec ident sz elems) next) = "\tthrust::transform(v" ++ show ident ++ ".begin(), v" ++ show ident ++ ".end(), " ++ fun ++ ");"
+    show (Decl (Vec ident sz elems) next) = "\tthrust::host_vector<type>v" 
+                                            ++ (show ident) 
+                                            ++ ";"
+    show (Trans fun (Vec ident sz elems) next) = "\tthrust::transform(v" 
+                                                  ++ show ident 
+                                                  ++ ".begin(), v" 
+                                                  ++ show ident 
+                                                  ++ ".end(), " 
+                                                  ++ fun 
+                                                  ++ ");"
 
 instance Show ElemType where
     show (CInt) = "int"    
 
 instance Show (Fun next) where
-    show (Proc t name args body next) = show t ++ " " ++ name ++
-                                        "(" ++ (foldr (\(t, name) acc -> 
-                                                        (show t ++ " " ++ name) ++ 
-                                                        case acc of
-                                                           ")" -> acc
-                                                           _ -> ", " ++ acc)
-                                                       ")" args) 
-                                               ++ "{"
+    show (Proc t name args body next) = show t 
+                                        ++ " " 
+                                        ++ name 
+                                        ++ "(" 
+                                        ++ (foldr (\(t, name) acc -> 
+                                              (show t 
+                                                ++ " " 
+                                                ++ name) 
+                                              ++ case acc of
+                                                ")" -> acc
+                                                _ -> ", " ++ acc)
+                                                ")" args) 
+                                        ++ "{"
 
 type Name = String
 
