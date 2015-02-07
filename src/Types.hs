9d15a6ae009065a270ab74cc44bff0ccd071791f
superfunc <superfunc@protonmail.ch>
2015-02-07 00:16:38 -0600

    start working on show instance, fix compiler errors

--- src/Types.hs
+++ src/Types.hs
@@ -1,18 +1,28 @@
-{-# LANGUAGE GADTs, KindSignatures, StandaloneDeriving, DeriveFunctor #-}
+{-# LANGUAGE GADTs , DeriveFunctor #-}
 
 module Types where
 
 import Control.Monad.State
 import Control.Monad.Free
 
-data Expr = Lit Integer | Add Expr Expr | Sub Expr Expr | Mult Expr Expr | Var String
-    deriving Show
+data Expr = Lit Integer 
+        | Add Expr Expr 
+        | Sub Expr Expr 
+        | Mult Expr Expr 
+        | Var String
 
 instance Num Expr where
-    fromInteger n = Lit n
-    e1 + e2 = Add e1 e2
-    e1 * e2 = Mult e1 e2
-    e1 - e2 = Sub e1 e2
+  fromInteger n = Lit n
+  e1 + e2 = Add e1 e2
+  e1 * e2 = Mult e1 e2
+  e1 - e2 = Sub e1 e2
+
+instance Show Expr where
+  show (Add e1 e2)  = "(" ++ show e1 ++ " + " ++ show e2 ++ ")" 
+  show (Sub e1 e2)  = "(" ++ show e1 ++ " + " ++ show e2 ++ ")" 
+  show (Mult e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")" 
+  show (Lit n)      = "(" ++ show n ++ ")"
+  show (Var s)      = "(" ++ s ++ ")"
 
 data HVector = Vec Int Int [(Int, Expr)]
     deriving Show
@@ -20,28 +30,30 @@ data HVector = Vec Int Int [(Int, Expr)]
 data ElemType where
     CInt :: ElemType
 
-data Statement next = Decl HVector next | Trans Expr HVector next
+data Statement next = Decl HVector next 
+                    | Trans Expr HVector next
     deriving (Functor)
 
 
 instance Show (Statement next) where
-    show (Decl (Vec ident sz elems) next) = "\tthrust::host_vector<type>v" 
+  show (Decl (Vec ident _ elems) next) = "\tthrust::host_vector<type>v" 
+                                          ++ (show ident) 
+                                          ++ ";\n\t"
+                                          ++ concatMap (\(ind, val) -> "v" 
                                             ++ (show ident) 
-                                            ++ ";\n\t"
-                                            ++ concatMap (\(ind, val) -> "v" 
-                                                                         ++ (show ident) 
-                                                                         ++ "[" 
-                                                                         ++ (show ind)
-                                                                         ++ "] = "
-                                                                         ++ (show val) 
-                                                                         ++ ";\n\t") elems
-    show (Trans fun (Vec ident sz elems) next) = "\tthrust::transform(v" 
-                                                  ++ show ident 
-                                                  ++ ".begin(), v" 
-                                                  ++ show ident 
-                                                  ++ ".end(), " 
-                                                  ++ show fun 
-                                                  ++ ");"
+                                            ++ "[" 
+                                            ++ (show ind)
+                                            ++ "] = "
+                                            ++ (show val) 
+                                            ++ ";\n\t") elems
+
+  show (Trans fun (Vec ident _ _) next) = "\tthrust::transform(v" 
+                                          ++ show ident 
+                                          ++ ".begin(), v" 
+                                          ++ show ident 
+                                          ++ ".end(), " 
+                                          ++ show fun 
+                                          ++ ");"
 
 instance Show ElemType where
     show (CInt) = "int"    
@@ -49,4 +61,3 @@ instance Show ElemType where
 type Stmt = Free Statement
 
 type Func = StateT Int Stmt
-
