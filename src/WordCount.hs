55af53ada5fd14638cf214e9a931d00378940c1b
superfunc <jfilstrup@ku.edu>
2015-02-11 20:17:10 -0600

    Update files

--- src/WordCount.hs
+++ src/WordCount.hs
@@ -3,19 +3,7 @@ module WordCount where
 import Types
 import Lang
 
--- wordCountEx
---
-
-isAlpha :: Expr -> Expr
-isAlpha = (\x -> (x > 'A') && (x < 'z')) 
-
-delim :: Expr -> Expr -> Expr
-delim = (\x y -> isAlpha x && isAlpha y)
-
-accum :: Expr -> Expr 
-accum :: (\x -> x + 1)
-
 wordCount :: Func ()
 wordCount = do res   <- Func DVector
-               count <- foldD delim accum 0 res
+               count <- foldD (\x y -> isAlpha x && isAlpha y) add 0 res
                putStrLn $ show count
