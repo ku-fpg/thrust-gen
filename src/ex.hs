0a879ba2e46654e43b67b9787e37b3f2ac8a65b3
superfunc <jfilstrup@ku.edu>
2015-02-14 01:22:31 -0600

    fixed example

--- src/ex.hs
+++ src/ex.hs
@@ -1,7 +1,7 @@
 import Lang
 import Types
 
-prog = do a <- vector 20 [(0,5), (1, 20), (2, 4)]
+prog = do a <- vector 20 [(0,I 5), (1, I 20), (2, I 4)]
           return a
 
-main = run (prog 0 1)
+main = run prog 
