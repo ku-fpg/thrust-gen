535e6792aecc6072beb78bbb82d5a27b164bb269
superfunc <jfilstrup@ku.edu>
2015-02-14 03:02:16 -0600

    reorg

--- thrust-gen-cli/thrust-gen.hs
+++ thrust-gen-cli/thrust-gen.hs
@@ -1,19 +1,12 @@
 module Main where
 
+import Types
+import Lang 
+
 import Prelude hiding (fromList)
 import System.Environment (getArgs)
 import Data.Map.Lazy as Map
 import Text.JSON.Parsec
 
-init :: IO ()
-
-build :: IO ()
-
-run :: IO ()
-
-help :: IO ()
-
-
 main :: IO ()
-main = do args <- getArgs           
-          return ()
+main = return ()
