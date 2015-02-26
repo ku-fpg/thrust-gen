0c0b74dadb4d7633d070c8ffd46014ea394a6eec
superfunc <superfunc@protonmail.ch>
2015-02-26 17:18:43 -0600

    fix compiler warnings

--- src/Ion/Prelude.hs
+++ src/Ion/Prelude.hs
@@ -1,9 +1,9 @@
 {-- Emulating key pieces of the thrust API -}
 
 module Ion.Prelude ( all_
-		   , any_
-		   , reduce
-		   ) where
+		                , any_
+		                , reduce
+		               ) where
 
 import Data.Boolean
 import Data.Complex
@@ -14,7 +14,7 @@ import Control.Monad.Free
 reduce :: (Show a) => (Expr a, (Expr b -> Expr b -> Expr a)) 
 	        -> Vector a 
 		      -> Ion (Vector a)
-reduce pr x = reduce' x (fst $ pr) (snd $ pr)
+reduce pr x = reduce__ x (fst $ pr) (snd $ pr)
 
 -- Using double underscore suffix to denote same-name helper functions,
 -- not to be exported
@@ -22,12 +22,12 @@ reduce__ :: (Show a) => Vector a -> Expr a
 	          -> (Expr b -> Expr c -> Expr a) 
 		        -> Ion (Vector a)
 reduce__ c initV expr = do p  <- newLabel
-                            p2 <- newLabel
-                            let body = (expr (Var "a")) (Var "b")
-                                to   = "v" ++ show p2
-                                name = "f" ++ show p
-                                func = CFunc name body Neither None StructBased 2
-                            liftF $ Fold to func c initV c
+                           p2 <- newLabel
+                           let body = (expr (Var "a")) (Var "b")
+                               to   = "v" ++ show p2
+                               name = "f" ++ show p
+                               func = CFunc name body Neither None StructBased 2
+                           liftF $ Fold to func c initV c
  
 all_ :: (Boolean b) => (b, (b -> b -> b))
 all_ = (true, (\x y -> x &&* y))
