a47f4246c95aa7043a0bab64a445392f619c0dee
mstees <mstees@ittc.ku.edu>
2015-03-03 14:44:53 -0600

    Histogram example in thrustgen

---
+++ examples/ThrustGenExamples/Histogram.hs
@@ -0,0 +1,14 @@
+import Ion.Base
+import Ion.Prelude
+
+main :: IO ()
+main = toThrust $ do f <- vector int [0,2,3,5]
+                     cout f
+                     --empty vector to store histogram counts in
+                     z <- vector int (take 6 $ cycle [0])
+                     a <- countingiterator int 0
+                     res <- upperbound f z a
+                     --actual histogram value
+                     res' <- adjdiff res
+                     cout res'
+                     return f
